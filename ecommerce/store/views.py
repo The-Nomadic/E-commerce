bc153e92de321610917daef327fec52cda3eb1b4
Rarij R <rarijr5144@gmail.com>
2023-11-12 21:16:48 +0530

    Created dynamis data

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from .models import Product
+from datetime import datetime
 
 
 # Create your views here.
 def home(request):
-    return render(request, "home.html", {})
+    current_year = datetime.now().year
+    products = Product.objects.all()
+    return render(request, "home.html", {'company': "New Gens", 'current_year': current_year, 'products': products})
