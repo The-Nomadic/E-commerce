4125ecc7b9d6efed5a388b4a2c12c0b07f20cc89
Rarij R <rarijr5144@gmail.com>
2023-11-13 22:31:50 +0530

    Made changes to templates, added base,about,navbar,footer,home

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -2,9 +2,14 @@ from django.shortcuts import render
 from .models import Product
 from datetime import datetime
 
+current_year = datetime.now().year
+
 
 # Create your views here.
 def home(request):
-    current_year = datetime.now().year
     products = Product.objects.all()
     return render(request, "home.html", {'company': "New Gens", 'current_year': current_year, 'products': products})
+
+
+def about(request):
+    return render(request, "about.html", {'company': "New Gens", 'current_year': current_year})
