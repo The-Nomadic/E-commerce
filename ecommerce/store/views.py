7d38f836305ab92e8d8be38e05547cba84fc95b8
Rarij R <rarijr5144@gmail.com>
2023-11-14 16:32:26 +0530

    Added login, logout, registration

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Product
 from datetime import datetime
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from .forms import SignUpForm
 
 current_year = datetime.now().year
 
@@ -13,3 +16,45 @@ def home(request):
 
 def about(request):
     return render(request, "about.html", {'company': "New Gens", 'current_year': current_year})
+
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Success")
+            return redirect('home')
+        else:
+            messages.success(request, "Something went wrong, Try again")
+            return redirect('login')
+    else:
+        return render(request, "login.html", {'company': "New Gens", 'current_year': current_year})
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "You have been logged out.")
+    return redirect('home')
+
+
+def register_user(request):
+    form = SignUpForm()
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            # lig in user
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "Account Created")
+            return redirect('home')
+        else:
+            messages.success(request, "Sorry, Some Error Occurred")
+            return redirect('register')
+    else:
+        return render(request, "register.html", {'company': "New Gens", 'current_year': current_year, 'form': form})
